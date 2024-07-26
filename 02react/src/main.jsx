0655aa3407ad4336a65b94c649daeb3c09ffe103
Janmejay Sahu <chikunsahu73@gmail.com>
2024-07-27 00:31:48 +0530

    day 4

--- 02react/src/main.jsx
+++ 02react/src/main.jsx
@@ -2,9 +2,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import Hello from './janu.jsx';
 
+function MyApp() {
+    return (
+      <h1>Hare krishna</h1>
+    )
+  }
+  export default MyApp;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <App />
+        <MyApp />
         <Hello />
     </>
 )
---
+++ 03react/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1> Hello </h1>
+      <h1> Counter Value:5 </h1>
+
+      <button>Add Value</button>
+      <button>remove Value</button>
+    </>
+  )
+}
+
+export default App
